3143408667ecce0dc1bc4eda090a5d867b6cf817
Daniel <dnl.side@gmail.com>
2026-09-06 12:54:56 +0900

    Add initial iOS application

---
+++ Taller de Dispositivos Móviles/Taller de Dispositivos Móviles/Taller de Dispositivos Móviles/Taller de Dispositivos Móviles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Taller de Dispositivos Móviles
+//
+//  Created by ハラ・ダニエル on 2026/09/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Taller de Dispositivos Móviles/Taller de Dispositivos Móviles/Taller_de_Dispositivos_Mo_vilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taller_de_Dispositivos_Mo_vilesApp.swift
+//  Taller de Dispositivos Móviles
+//
+//  Created by ハラ・ダニエル on 2026/09/06.
+//
+
+import SwiftUI
+
+@main
+struct Taller_de_Dispositivos_Mo_vilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
