7fa5c7772b97e1e9d3738ca04a2cd4c65ad2655e
ダニエル <159549573+dnl-side@users.noreply.github.com>
2026-09-06 14:26:54 +0900

    Connect application dependency container

--- Taller de Dispositivos Móviles/Taller de Dispositivos Móviles/App/Taller_de_Dispositivos_Mo_vilesApp.swift
+++ Taller de Dispositivos Móviles/Taller de Dispositivos Móviles/App/Taller_de_Dispositivos_Mo_vilesApp.swift
@@ -2,22 +2,23 @@ import SwiftUI
 
 @main
 struct Taller_de_Dispositivos_Mo_vilesApp: App {
-    private let viewModel: StudentListViewModel
+    private let container: DependencyContainer
+    private let studentViewModel: StudentListViewModel
+    private let dashboardViewModel: DanceDashboardViewModel
 
     init() {
-        let localDataSource = LocalStudentDataSource()
-        let remoteDataSource = RemoteStudentDataSource()
-        let repository = StudentRepositoryImpl(
-            localDataSource: localDataSource,
-            remoteDataSource: remoteDataSource
-        )
-
-        viewModel = StudentListViewModel(repository: repository)
+        let container = DependencyContainer()
+        self.container = container
+        studentViewModel = container.makeStudentListViewModel()
+        dashboardViewModel = container.makeDanceDashboardViewModel()
     }
 
     var body: some Scene {
         WindowGroup {
-            StudentListView(viewModel: viewModel)
+            DanceSchoolView(
+                studentViewModel: studentViewModel,
+                dashboardViewModel: dashboardViewModel
+            )
         }
     }
 }
